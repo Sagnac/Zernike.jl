f72a2c1ad0df9801e621bff2acbbbd92875af83f
Sagnac <83491030+Sagnac@users.noreply.github.com>
2023-06-09 19:56:14 +0000

    TransformAperture: construct the translation matrix
    This requires a two-pass system as access to as of yet unmapped indices
    is required to construct the translation matrix for each iteration. This
    isn't much of a problem however as there isn't much of a performance
    penalty for looping through twice since remap doesn't do much work. Note
    that we can get away with not presetting the mapping for diagonal or
    block-diagonal matrices like the pure scale matrix or the coefficient
    matrix.
    
    The pure scale matrix is kept because even though the translation matrix
    covers scaling as well for zero shift it's a bit slower since it
    requires another inner loop and extra computation. Once the rotation
    matrix is included I will probably add a few extra conditions to ensure
    these extra computations aren't carried out unnecessarily.

--- src/TransformAperture.jl
+++ src/TransformAperture.jl
@@ -1,33 +1,54 @@
-function transform(ε::Float64, v::Vector{Float64})
+const b = binomial
+const ℯ = cis # ℯ(x) = exp(im*x)
+
+function transform(ε::T, δ::T, ϕ::T, v::Vector{T}) where T <: Float64
     len = length(v)
     n_max = get_n(len - 1)
     order = Tuple{Int, Int, Int}[]
-    remap = Dict{Tuple{Int, Int}, Int}()
+    mapping = remap(n_max)
     N = zeros(Float64, len, len)
     R = copy(N)
-    η = copy(N)
+    ηₛ = copy(N)
+    ηₜ = zeros(Complex{Float64}, len, len)
     i = 1
     for m = -n_max:n_max
         μ = abs(m)
         for n = μ:2:n_max
             j = get_j(m, n)
-            k = (n - μ) ÷ 2
-            push!(remap, (m, n) => i)
+            k1 = (n - μ) ÷ 2
             push!(order, (j + 1, get_j(-m, n) + 1, sign(m)))
             N[i,i] = √(n+1)
             λ = Φ(μ, n)
-            for s = 0:k
-                setindex!(R, λ[n-2s+1], remap[(m, n-2s)], i)
+            for s = 0:k1
+                setindex!(R, λ[n-2s+1], mapping[(m, n-2s)], i)
             end
-            η[i,i] = ε ^ n
+            ηₛ[i,i] = ε ^ n
+            k2 = (n + m) ÷ 2
+            k3 = (n - m) ÷ 2
+            for p = 0:k2, q = 0:k3
+                n′ = n - p - q
+                m′ = m - p + q
+                z = b(k2,p) * b(k3,q) * ε^n′ * δ^(p+q) * ℯ((p-q)ϕ)
+                setindex!(ηₜ, z, mapping[(m′, n′)], i)
+            end
             i += 1
         end
     end
     c = to_complex(v, order)
-    C = (R * N) \ (η * R * N)
+    C = (R * N) \ (ηₜ * R * N)
     c′ = C * c
     v2 = to_real(c′, order)
     return v2
+end
+
+function remap(n_max)
+    mapping = Dict{Tuple{Int, Int}, Int}()
+    i = 1
+    for m = -n_max:n_max, n = abs(m):2:n_max
+        push!(mapping, (m, n) => i)
+        i += 1
+    end
+    return mapping
 end
 
 function to_complex(v::Vector{Float64}, order::Vector{Tuple{Int, Int, Int}})
