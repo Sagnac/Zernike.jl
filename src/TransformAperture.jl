b9bf7cbe73e13f1523d2bd0a08d4ef1b97810501
Sagnac <83491030+Sagnac@users.noreply.github.com>
2023-06-09 03:59:18 +0000

    TransformAperture: separate functions for the real & complex conversions
    They're not exactly useful on their own as the order vector must be
    constructed within the loop and it has a very specific format, but doing
    this should keep the main algorithm cleaner.

--- src/TransformAperture.jl
+++ src/TransformAperture.jl
@@ -1,42 +1,42 @@
-function transform(v)
-    j_max::Int = length(v) - 1
-    n_max::Int = get_n(j_max)
-    order = []
+function transform(v::Vector{Float64})
+    n_max = get_n(length(v) - 1)
+    order = Tuple{Int, Int, Int}[]
     for m = -n_max:n_max
-        μ = abs(m)
-        for n = μ:2:n_max
+        for n = abs(m):2:n_max
             j = get_j(m, n)
-            if m != 0
-                push!(order, (j + 1, sign(m), get_j(-m, n) + 1))
-            else
-                push!(order, (j + 1, 0))
-            end
+            push!(order, (j + 1, get_j(-m, n) + 1, sign(m)))
         end
     end
-    c = Vector{Complex{Float64}}(undef, j_max + 1) 
+    c = to_complex(v, order)
+    v2 = to_real(c, order)
+    return c, v2
+end
+
+function to_complex(v::Vector{Float64}, order::Vector{Tuple{Int, Int, Int}})
+    c = Vector{Complex{Float64}}(undef, length(v)) 
     for (i, j) in pairs(order)
-        if j[2] < 0
-            c[i] = (v[j[3]] + im * v[j[1]]) / √2
-        elseif j[2] > 0
-            c[i] = (v[j[1]] - im * v[j[3]]) / √2
+        if j[3] < 0
+            c[i] = (v[j[2]] + im * v[j[1]]) / √2
+        elseif j[3] > 0
+            c[i] = (v[j[1]] - im * v[j[2]]) / √2
         else
             c[i] = v[j[1]]
         end
     end
-    v2 = Vector{Float64}(undef, j_max + 1)
-    c2 = Vector{Complex{Float64}}(undef, j_max + 1) 
+    return c
+end
+
+function to_real(c::Vector{Complex{Float64}}, order::Vector{Tuple{Int, Int, Int}})
+    c2 = c[sortperm(order; by = first)]
+    v2 = Vector{Float64}(undef, length(c2))
     for (i, j) in pairs(order)
-        c2[j[1]] = c[i]
-    end
-    permute!(c, sortperm(order; by = first))
-    for (i, j) in pairs(order)
-        if j[2] < 0
-            v2[j[1]] = (c[j[1]] - c[j[3]])  / √2 |> imag
-        elseif j[2] > 0
-            v2[j[1]] = (c[j[1]] + c[j[3]]) / √2 |> real
+        if j[3] < 0
+            v2[j[1]] = (c2[j[1]] - c2[j[2]])  / √2 |> imag
+        elseif j[3] > 0
+            v2[j[1]] = (c2[j[1]] + c2[j[2]]) / √2 |> real
         else
-            v2[j[1]] = c[j[1]] |> real
+            v2[j[1]] = c2[j[1]] |> real
         end
     end
-    return c, order, v2
+    return v2
 end
