5fdf45eade4b6aaeebda03f0b5b5c0f34b5d3fea
Sagnac <83491030+Sagnac@users.noreply.github.com>
2023-06-09 04:35:15 +0000

    TransformAperture: compute the N & R matrices
    So instead of utilizing a permutation matrix to switch the order
    such that R is a block-diagonal matrix I've decided to utilize an
    indexing scheme. Although this is non-standard, for me it's more
    intuitive, but I can see this being somewhat confusing for anyone
    reading through the source for the first time.
    
    The paper I'm basing this algorithm on (Lundström & Unsbo 2007) does not
    do it this way, but the MATLAB code they provide is somewhat difficult
    for me to follow (it's been years since I've written MATLAB code) and
    Julia allows for a more elegant (and possibly more efficient) approach.
    
    That being said, I'm not going to try to benchmark their implementation
    against mine; it's quite a well written paper that their mathematical
    presentation of the method is clear enough that it makes custom
    implementations straight-forward and this should be fast enough.
    
    All that's left is to compute the transformation matrices and then
    interface the functions.

--- src/TransformAperture.jl
+++ src/TransformAperture.jl
@@ -1,15 +1,27 @@
 function transform(v::Vector{Float64})
-    n_max = get_n(length(v) - 1)
+    j_max = length(v) - 1
+    n_max = get_n(j_max)
     order = Tuple{Int, Int, Int}[]
+    remap = Dict{Tuple{Int, Int}, Int}()
+    N = zeros(Float64, j_max + 1, j_max + 1)
+    R = zeros(Float64, j_max + 1, j_max + 1)
+    i = 1
     for m = -n_max:n_max
-        for n = abs(m):2:n_max
+        μ = abs(m)
+        for n = μ:2:n_max
             j = get_j(m, n)
+            k = (n - μ) ÷ 2
+            push!(remap, (m, n) => i)
             push!(order, (j + 1, get_j(-m, n) + 1, sign(m)))
+            N[i,i] = √(n+1)
+            λ = Φ(μ, n)
+            for s = 0:k
+                setindex!(R, λ[n-2s+1], remap[(m, n-2s)], i)
+            end
+            i += 1
         end
     end
-    c = to_complex(v, order)
-    v2 = to_real(c, order)
-    return c, v2
+    return N, R
 end
 
 function to_complex(v::Vector{Float64}, order::Vector{Tuple{Int, Int, Int}})
