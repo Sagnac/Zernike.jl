09adbf9e3e494bb5a613968a7de17f76d098ac90
Sagnac <83491030+Sagnac@users.noreply.github.com>
2023-06-11 19:16:58 +0000

    TransformAperture: create a separate function for translation
    Okay so if we're going to have a two-pass system this makes a lot more
    sense. To be specific, the coefficient matrix does not need a preset
    mapping since for each iteration the m map is fixed and what is
    essentially n′ (n-2s) only decreases (< n) from within the inner-most
    loop while the upper-level nested loop increments n; this means that
    there will always be a mapping available from a previous iteration.
    
    In contrast, the translation matrix's loop varies both m′ and n′ so we
    need to go through twice. But if we're going to do that instead of
    having a separate remap function a better way is to have a dedicated
    routine for the entire translation process since it's the real reason
    why we need another loop in the first place.

--- src/TransformAperture.jl
+++ src/TransformAperture.jl
@@ -4,14 +4,12 @@ const ℯ = cis # ℯ(x) = exp(im*x)
 function transform(ε::T, δ::Complex{T}, ϕ::T, v::Vector{T}) where T <: Float64
     len = length(v)
     n_max = get_n(len - 1)
+    remap = Dict{Tuple{Int, Int}, Int}()
     order = Tuple{Int, Int, Int}[]
-    mapping = remap(n_max)
     N = zeros(Float64, len, len)
     R = copy(N)
     if iszero(δ)
         ηₛ = copy(N)
-    else
-        ηₜ = zeros(Complex{T}, len, len)
     end
     if !iszero(ϕ)
         ηᵣ = zeros(ComplexF64, len, len)
@@ -23,31 +21,23 @@ function transform(ε::T, δ::Complex{T}, ϕ::T, v::Vector{T}) where T <: Float6
             i += 1
             j = get_j(m, n)
             k1 = (n - μ) ÷ 2
+            push!(remap, (m, n) => i)
             push!(order, (j + 1, get_j(-m, n) + 1, sign(m)))
             N[i,i] = √(n+1)
             λ = Φ(μ, n)
             for s = 0:k1
-                setindex!(R, λ[n-2s+1], mapping[(m, n-2s)], i)
+                setindex!(R, λ[n-2s+1], remap[(m, n-2s)], i)
             end
             if !iszero(ϕ)
                 ηᵣ[i,i] = ℯ(m * ϕ)
             end
             if iszero(δ)
                 ηₛ[i,i] = ε ^ n
-                continue
-            end
-            ρₜ, θₜ = abs(δ), angle(δ)
-            k2 = (n + m) ÷ 2
-            k3 = (n - m) ÷ 2
-            for p = 0:k2, q = 0:k3
-                n′ = n - p - q
-                m′ = m - p + q
-                z = b(k2,p) * b(k3,q) * ε^n′ * ρₜ^(p+q) * ℯ((p-q)θₜ)
-                setindex!(ηₜ, z, mapping[(m′, n′)], i)
             end
         end
     end
     if !iszero(δ)
+        ηₜ = translate(ε, δ, n_max, remap)
         if !iszero(ϕ)
             η = ηᵣ * ηₜ
         else
@@ -65,14 +55,23 @@ function transform(ε::T, δ::Complex{T}, ϕ::T, v::Vector{T}) where T <: Float6
     return v2
 end
 
-function remap(n_max)
-    mapping = Dict{Tuple{Int, Int}, Int}()
-    i = 1
+function translate(ε::Float64, δ::ComplexF64, n_max::Int, remap::Dict)
+    ρₜ, θₜ = abs(δ), angle(δ)
+    len = length(remap)
+    ηₜ = zeros(ComplexF64, len, len)
+    i = 0
     for m = -n_max:n_max, n = abs(m):2:n_max
-        push!(mapping, (m, n) => i)
         i += 1
+        k2 = (n + m) ÷ 2
+        k3 = (n - m) ÷ 2
+        for p = 0:k2, q = 0:k3
+            n′ = n - p - q
+            m′ = m - p + q
+            z = b(k2,p) * b(k3,q) * ε^n′ * ρₜ^(p+q) * ℯ((p-q)θₜ)
+            setindex!(ηₜ, z, remap[(m′, n′)], i)
+        end
     end
-    return mapping
+    return ηₜ
 end
 
 function to_complex(v::Vector{Float64}, order::Vector{Tuple{Int, Int, Int}})
