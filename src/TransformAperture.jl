e33a8a749327a470b0e60e7d5b9e71ff51a2aaea
Sagnac <83491030+Sagnac@users.noreply.github.com>
2023-06-13 10:50:20 +0000

    TransformAperture: integrate the elliptical transform into the algorithm

--- src/TransformAperture.jl
+++ src/TransformAperture.jl
@@ -8,11 +8,15 @@ https://opg.optica.org/josaa/abstract.cfm?uri=josaa-24-3-569
 
 =#
 
+using LinearAlgebra
+
 const b = binomial
 const ℯ = cis # ℯ(x) = exp(im*x)
 
-function transform(v::Vector{T}, ε::T, δ::Complex{T}, ϕ::T) where T <: Float64
+function transform(v::Vector{T}, ε::T,
+                   δ::Complex{T}, ϕ::T, ω::Tuple{T, T}) where T <: Float64
     !(0.0 ≤ ε + abs(δ) ≤ 1.0) && error("Bounds: 0.0 ≤ ε + abs(δ) ≤ 1.0\n")
+    !(0.0 ≤ ω[1] ≤ 1.0) && error("Bounds: 0.0 ≤ ω[1] ≤ 1.0\n")
     len = length(v)
     n_max = get_n(len - 1)
     remap = Dict{Tuple{Int, Int}, Int}()
@@ -21,14 +25,12 @@ function transform(v::Vector{T}, ε::T, δ::Complex{T}, ϕ::T) where T <: Float6
     N = zeros(Float64, len, len)
     # radial coefficient block-diagonal matrix
     R = copy(N)
+    # scaling
     if iszero(δ)
-        # scaling
         ηₛ = copy(N)
     end
-    if !iszero(ϕ)
-        # rotation
-        ηᵣ = zeros(ComplexF64, len, len)
-    end
+    # rotation
+    ηᵣ = !iszero(ϕ) ? zeros(ComplexF64, len, len) : I
     i = 0
     for m = -n_max:n_max
         μ = abs(m)
@@ -51,18 +53,12 @@ function transform(v::Vector{T}, ε::T, δ::Complex{T}, ϕ::T) where T <: Float6
         end
     end
     if !iszero(δ)
-        ηₜ = translate(ε, δ, remap)
-        if !iszero(ϕ)
-            # net transformation matrix
-            η = ηᵣ * ηₜ
-        else
-            η = ηₜ
-        end
-    elseif !iszero(ϕ)
-        η = ηᵣ * ηₛ
-    else
-        η = ηₛ
+        ηₛ = translate(ε, δ, remap)
     end
+    ηₑ = !iszero(ω[1]) ? elliptical(ω..., remap) : I
+    # net transformation matrix
+    η = ηₑ * ηᵣ * ηₛ
+    # complex Zernike expansion coefficients
     c = to_complex(v, order)
     # conversion matrix
     C = (R * N) \ (η * R * N)
@@ -93,17 +89,10 @@ function translate(ε::Float64, δ::ComplexF64, remap::Dict)
     return ηₜ
 end
 
-function elliptical(v::Vector{T}, ξ::T, φ::T) where T <: Float64
-    !(0.0 ≤ ξ ≤ 1.0) && error("Bounds: 0.0 ≤ ξ ≤ 1.0\n")
-    len = length(v)
+function elliptical(ξ::Float64, φ::Float64, remap::Dict)
+    len = length(remap)
     n_max = get_n(len - 1)
-    remap = Dict{Tuple{Int, Int}, Int}()
     ηₑ = zeros(ComplexF64, len, len)
-    i = 0
-    for m = -n_max:n_max, n = abs(m):2:n_max
-        i += 1
-        push!(remap, (m, n) => i)
-    end
     for m = -n_max:n_max, n = abs(m):2:n_max
         k2 = (n + m) ÷ 2
         k3 = (n - m) ÷ 2
@@ -147,9 +136,10 @@ function to_real(c::Vector{Complex{Float64}}, order::Vector{Tuple{Int, Int, Int}
     return v2
 end
 
-function S(v::Vector{T}, ε::T, δ::Complex{T} = 0.0im, ϕ::T = 0.0;
+function S(v::Vector{T}, ε::T, δ::Complex{T} = 0.0im,
+           ϕ::T = 0.0, ω::Tuple{T, T} = (0.0, 0.0);
            precision = 3, scale::Int = 101) where T <: Float64
-    v2, n_max = transform(v, ε, δ, ϕ)
+    v2, n_max = transform(v, ε, δ, ϕ, ω)
     Zᵢ = Vector{Polynomial}(undef, length(v))
     ΔW, b = Ψ(v2, Zᵢ, n_max; precision)
     Λ(ΔW, b, v2, n_max; scale)
@@ -157,21 +147,28 @@ end
 
 function S(v::Vector{T}, ε::T, ::Model;
            precision = 3) where T <: Float64
-    v2, n_max = transform(v, ε, 0.0im, zero(T))
+    v2, n_max = transform(v, ε, 0.0im, zero(T), (0.0, 0.0))
     Zᵢ = Vector{Polynomial}(undef, length(v))
     Ψ(v2, Zᵢ, n_max; precision)[1]
 end
 
 function S(v::Vector{T}, ε::T, δ::Complex{T}, ::Model;
            precision = 3) where T <: Float64
-    v2, n_max = transform(v, ε, δ, zero(T))
+    v2, n_max = transform(v, ε, δ, zero(T), (0.0, 0.0))
     Zᵢ = Vector{Polynomial}(undef, length(v))
     Ψ(v2, Zᵢ, n_max; precision)[1]
 end
 
 function S(v::Vector{T}, ε::T, δ::Complex{T}, ϕ::T, ::Model;
            precision = 3) where T <: Float64
-    v2, n_max = transform(v, ε, δ, ϕ)
+    v2, n_max = transform(v, ε, δ, ϕ, (0.0, 0.0))
+    Zᵢ = Vector{Polynomial}(undef, length(v))
+    Ψ(v2, Zᵢ, n_max; precision)[1]
+end
+
+function S(v::Vector{T}, ε::T, δ::Complex{T}, ϕ::T, ω::Tuple{T, T}, ::Model;
+           precision = 3) where T <: Float64
+    v2, n_max = transform(v, ε, δ, ϕ, ω)
     Zᵢ = Vector{Polynomial}(undef, length(v))
     Ψ(v2, Zᵢ, n_max; precision)[1]
 end
