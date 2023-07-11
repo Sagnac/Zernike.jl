76e1564bf7aa85f330b767e5517db6cb395ceefe
Sagnac <83491030+Sagnac@users.noreply.github.com>
2023-07-11 10:08:26 +0000

    v3.1.0

--- src/Zernike.jl
+++ src/Zernike.jl
@@ -1,6 +1,6 @@
 # Zernike.jl
-# Version 3.0.0
-# 2023-7-3
+# Version 3.1.0
+# 2023-7-11
 # https://github.com/Sagnac/Zernike.jl
 
 # Generates Zernike polynomials, models wavefront errors, and plots them in Makie.
