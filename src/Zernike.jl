669acfd759214af5baa625740a7812422cd2fee2
Sagnac <83491030+Sagnac@users.noreply.github.com>
2024-09-28 18:38:35 +0000

    Ensure compatibility with Julia 1.10

--- src/Zernike.jl
+++ src/Zernike.jl
@@ -11,8 +11,11 @@ export zernike, wavefront, transform, Z, W, P, WavefrontError,
        noll_to_j, fringe_to_j, standardize, standardize!,
        Observable, plotconfig, zplot
 
-# public radial_coefficients, wavefront_coefficients, transform_coefficients,
-       # metrics, scale, J, Superposition, Product
+const public_names = "public \
+    radial_coefficients, wavefront_coefficients, transform_coefficients, \
+    metrics, scale, J, Superposition, Product"
+
+VERSION >= v"1.11.0-DEV.469" && eval(Meta.parse(public_names))
 
 using GLMakie
 import .Makie: latexstring, LaTeXString, FigureAxisPlot
