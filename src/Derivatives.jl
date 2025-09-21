664782614040f9a3e15cbb72a21aaa202614fb87
Sagnac <83491030+Sagnac@users.noreply.github.com>
2025-09-21 05:29:34 +0000

    Cartesian derivatives in a Zernike basis
    This seems to work from my limited testing, but it needs rigorous
    checking along with the rest of the derivative code. I'll write the test
    suite for it and document it when I add the Laplacian algorithm.

--- src/Derivatives.jl
+++ src/Derivatives.jl
@@ -70,3 +70,68 @@ function show(io::IO, ∂::T) where T <: Derivative
 end
 
 show(io::IO, ::T) where T <: Union{Gradient, Laplacian} = print(io, T)
+
+function Wavefront(g::Gradient)
+    (; m, n) = g.r.inds
+    Wavefront(Gradient, m, n)
+end
+
+function Wavefront(::Type{<:Gradient}, m::Int, n::Int)
+    cx, cy = grad(m, n)
+    order = conjugate_indices(n - 1)
+    ax = to_real(cx, order, 1)
+    ay = to_real(cy, order, 1)
+    ∂x = Wavefront(ax)
+    ∂y = Wavefront(ay)
+    return ∂x, ∂y
+end
+
+function conjugate_indices(n_max::Int)
+    order = Vector{NTuple{3}{Int}}(undef, get_j(max(n_max, 0)) + 1)
+    for i in eachindex(order)
+        m, n = get_mn(i - 1)
+        order[i] = (i, get_j(-m, n) + 1, sign(m))
+    end
+    return order
+end
+
+#= The following algorithm computes Cartesian derivatives in terms of Zernike polynomials. It is based on mathematical formulas found in:
+
+https://doi.org/10.1364/JOSAA.31.001604
+
+A. J. E. M. Janssen, "Zernike expansion of derivatives and Laplacians of the Zernike circle polynomials," J. Opt. Soc. Am. A 31, 1604-1613 (2014)
+
+https://opg.optica.org/josaa/abstract.cfm?uri=josaa-31-7-1604
+
+Note:
+
+Complex polynomials correspond to a linear combination of two real, standard
+Zernike polynomials. As such, derivatives must be computed for conjugate indices
+and appropriately combined before converting the complex coefficients to
+the real ones.
+
+=#
+
+function grad(m::Int, n::Int)
+    len = get_j(max(n - 1, 0)) + 1
+    c = [zeros(ComplexF64, len) for i = 1:4]
+    # c[1:2] ≡ ∂/∂x (Z(±m, n)), c[3:4] ≡ ∂/∂y (Z(±m, n))
+    for s = 0:div(n - abs(m), 2)
+        cs = n - 2s
+        n′ = cs - 1
+        n′ < 0 && break
+        μ = abs(m)
+        for (i, m′) ∈ enumerate(((μ + 1, μ - 1), (-μ + 1, -μ - 1)))
+            for (i′, m′′) ∈ enumerate(m′)
+                if abs(m′′) ≤ n′
+                    i′′ = get_j(m′′, n′) + 1
+                    c[i][i′′] = cs
+                    c[i+2][i′′] = psgn(i′) * im * cs
+                end
+            end
+        end
+    end
+    op, d = m < 0 ? ((-), im) : ((+), 1)
+    cx, cy = (op(c[i], c[i+1]) / 2d for i = (1, 3))
+    return cx, cy, c
+end
