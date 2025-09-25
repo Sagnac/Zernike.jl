Sagnac/Zernike.jl

commit d36fc96d63f65248955ba0d6722eba5c6da327b3

Properly implement `setindex!` for `Wavefront`
These two vectors also need to be updated, especially `a` since those
are the coefficients which are used to compute values over the pupil.

Maybe this shouldn't be allowed, given how the fields are somewhat
dependent; once you update a value the wavefront is no longer the result
of fitting, but of course Wavefronts are not necessarily always the
result of fitting so given the general manipulations allowed this fits
the theme and this scheme can be quite useful.

src/Wavefront.jl
@@ -238,7 +238,15 @@ firstindex(W::Wavefront) = 0
 
 lastindex(W::Wavefront) = lastindex(W.v) - 1
 
-setindex!(W::Wavefront, x, j) = (W.v[j.+1] = x)
+function setindex!(W::Wavefront, x, j)
+    for (i, t) in pairs(W.recap)
+        if t.j == j
+            W.recap[i] = merge(t, (; a = x))
+        end
+    end
+    W.a[j.+1] = x
+    W.v[j.+1] = x
+end
 
 # reduces precision
 function reduce_wave(W::Wavefront, precision::Int)

commit 8413f30269e3161e4a062e6ae8572554e4614e6b

Wavefront: extend `getindex` & `setindex!` for `m`, `n` indices
Also, fix an out of bounds access bug introduced in d36fc96.

src/Wavefront.jl
@@ -234,6 +234,12 @@ getindex(W::Wavefront) = W.v
 
 getindex(W::Wavefront, j) = W.v[j.+1]
 
+getindex(W::Wavefront, m::Int, n::Int) = W[get_j(m, n)]
+
+getindex(W::Wavefront, mn::NTuple{2}{Int}) = W[mn[1], mn[2]]
+
+getindex(W::Wavefront, orders::Vector{NTuple{2}{Int}}) = W[get_j.(orders)]
+
 firstindex(W::Wavefront) = 0
 
 lastindex(W::Wavefront) = lastindex(W.v) - 1
@@ -242,11 +248,17 @@ function setindex!(W::Wavefront, x, j)
     for (i, t) in pairs(W.recap)
         if t.j == j
             W.recap[i] = merge(t, (; a = x))
+            W.a[i] = x
         end
     end
-    W.a[j.+1] = x
     W.v[j.+1] = x
 end
+
+setindex!(W::Wavefront, x, m::Int, n::Int) = setindex!(W, x, get_j(m, n))
+
+setindex!(W::Wavefront, x, mn::NTuple{2}{Int}) = (W[get_j(mn)] = x)
+
+setindex!(W::Wavefront, x, orders::Vector{NTuple{2}{Int}}) = (W[get_j.(orders)] = x)
 
 # reduces precision
 function reduce_wave(W::Wavefront, precision::Int)